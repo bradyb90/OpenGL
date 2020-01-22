0d105fdae11ecdcf26d755a42fe67e2e627f3d6c
Brady <bradyb90@knights.ucf.edu>
2020-01-22 18:45:42 -0500

    Added imgui, this created a gui that changes the x,y,z of the texture we added in previous commits (clover.png).

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -15,6 +15,11 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 int main(void)
 {
    GLFWwindow* window;
@@ -51,10 +56,10 @@ int main(void)
    {
       float positions[] =
       {
-         -0.5f, -0.5f, 0.0f, 0.0f,
-          0.5f, -0.5f, 1.0f, 0.0f,
-          0.5f,  0.5f, 1.0f, 1.0f,
-         -0.5f,  0.5f, 0.0f, 1.0f
+          100.0f, 100.0f, 0.0f, 0.0f,
+          200.0f, 100.0f, 1.0f, 0.0f,
+          200.0f, 200.0f, 1.0f, 1.0f,
+          100.0f, 200.0f, 0.0f, 1.0f
       };
 
       unsigned int indices[] =
@@ -76,11 +81,14 @@ int main(void)
 
       IndexBuffer ib(indices, 6);
 
+      glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+      glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+
       Shader shader("res/shaders/Basic.shader");
       shader.Bind();
       shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
-      Texture texture("res/textures/file.png");
+      Texture texture("res/textures/clover.png");
       texture.Bind();
       shader.SetUniform1i("u_Texture", 0);
 
@@ -91,6 +99,12 @@ int main(void)
 
       Renderer renderer;
 
+      ImGui::CreateContext();
+      ImGui_ImplGlfwGL3_Init(window, true);
+      ImGui::StyleColorsDark();
+
+      glm::vec3 translation(200, 200, 0);
+
       float r = 0.0f;
       float increment = 0.05f;
       /* Loop until the user closes the window */
@@ -99,8 +113,14 @@ int main(void)
          /* Render here */
          renderer.Clear();
 
+         ImGui_ImplGlfwGL3_NewFrame();
+
+         glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+         glm::mat4 mvp = proj * view * model;
+
          shader.Bind();
          shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+         shader.SetUniformMat4f("u_MVP", mvp);
 
          renderer.Draw(va, ib, shader);
 
@@ -115,6 +135,15 @@ int main(void)
 
          r += increment;
 
+         {
+
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+         }
+
+         ImGui::Render();
+         ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
          /* Swap front and back buffers */
          glfwSwapBuffers(window);
 
@@ -124,6 +153,8 @@ int main(void)
    }
 
    //GLCall(glfwTerminate());
+   ImGui_ImplGlfwGL3_Shutdown();
+   ImGui::DestroyContext();
    glfwTerminate();
    return 0;
 }
